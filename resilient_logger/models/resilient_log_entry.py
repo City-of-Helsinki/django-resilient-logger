db1d35b8ddce287193da87c0812b0dd0998349ff
Kalle Jokinen <kalle.jokinen@leasedealit.fi>
2025-10-29 14:11:13 +0200

    feat: remove explicit id field from model

--- resilient_logger/models/resilient_log_entry.py
+++ resilient_logger/models/resilient_log_entry.py
@@ -3,7 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ResilientLogEntry(models.Model):
-    id = models.AutoField(primary_key=True)
     is_sent = models.BooleanField(default=False, verbose_name=_("is sent"))
     level = models.IntegerField(verbose_name=_("level"), default=0)
     message = models.JSONField(verbose_name=_("message"))
