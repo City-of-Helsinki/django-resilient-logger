7299ca585473e10474f577f2c84d540f4d40032c
Kalle Jokinen <kalle.jokinen@leasedealit.fi>
2026-09-25 12:56:26 +0300

    fix: refactor code to address thread safety

--- resilient_logger/workarounds/models.py
+++ resilient_logger/workarounds/models.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from contextvars import ContextVar
 
 import auditlog.models
 from auditlog.models import LogEntry, LogEntryManager
@@ -6,11 +7,14 @@ from auditlog.models import LogEntry, LogEntryManager
 from resilient_logger.workarounds.types import ObjectReprFn
 from resilient_logger.workarounds.utils import resolve_object_repr_fn
 
+_object_repr_var: ContextVar[ObjectReprFn | None] = ContextVar(
+    "_object_repr_var", default=None
+)
+
 
 class DjangoAuditLogEntryManager(LogEntryManager):
     def __init__(self, object_repr_fn: ObjectReprFn):
         self._object_repr_fn = object_repr_fn
-        self._original_smart_str = auditlog.models.smart_str
         super().__init__()
 
     def log_create(self, instance, force_log: bool = False, **kwargs):
@@ -27,12 +31,12 @@ class DjangoAuditLogEntryManager(LogEntryManager):
 
     @contextmanager
     def _custom_repr_fn(self):
-        auditlog.models.smart_str = self._object_repr_fn
+        token = _object_repr_var.set(self._object_repr_fn)
 
         try:
             yield
         finally:
-            auditlog.models.smart_str = self._original_smart_str
+            _object_repr_var.reset(token)
 
     @staticmethod
     def patch(object_repr_fn: ObjectReprFn | None = None):
@@ -40,6 +44,15 @@ class DjangoAuditLogEntryManager(LogEntryManager):
         original_objects = LogEntry.objects
         original_base = LogEntry._meta.base_manager
         original_default = LogEntry._meta.default_manager
+        original_smart_str = auditlog.models.smart_str
+
+        def _smart_str_proxy(s):
+            custom_fn = _object_repr_var.get()
+
+            if custom_fn is not None:
+                return custom_fn(s)
+
+            return original_smart_str(s)
 
         manager = DjangoAuditLogEntryManager(object_repr_fn or resolve_object_repr_fn())
         manager.model = LogEntry
@@ -47,10 +60,12 @@ class DjangoAuditLogEntryManager(LogEntryManager):
         LogEntry.objects = manager
         LogEntry._meta.base_manager = manager
         LogEntry._meta.default_manager = manager
+        auditlog.models.smart_str = _smart_str_proxy
 
         def restore():
             LogEntry.objects = original_objects
             LogEntry._meta.base_manager = original_base
             LogEntry._meta.default_manager = original_default
+            auditlog.models.smart_str = original_smart_str
 
         return restore
